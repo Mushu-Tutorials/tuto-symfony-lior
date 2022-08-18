cc70d29c8f0f8433f40db6ecfbc00e396467f7b9
MushuLeDragon <dragon.mushu@hotmail.com>
2022-08-18 14:14:06 +0200

    include css

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -20,7 +20,7 @@ class BlogController extends AbstractController
     public function home()
     {
         return $this->render('blog/home.html.twig', [
-            'title' => 'Yolo'
+            'title' => 'Bienvenue les amis !'
         ]);
     }
 }
